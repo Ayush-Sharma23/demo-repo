2fc90be196959b26a2cc6a9e9a99398dccd99961
SAKSHIJV2 <145011004+SAKSHIJV2@users.noreply.github.com>
2025-01-23 12:30:17 +0530

    Update star.cpp

--- Activity-1/star.cpp
+++ Activity-1/star.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     for (int i = 1; i <= 5; i++) {
-        for (int j = 1; j <= 5; j++) {
+        for (int j = i; j <= 5; j++) {
             cout << "*";
         }
         cout << endl;
